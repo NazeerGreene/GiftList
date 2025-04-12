8a498fdcfed5febebb6bd0d6d5ddd301f2c55712
Nazeer Greene <nazeerg5@gmail.com>
2025-04-12 17:44:10 -0500

    solutions add

--- client/index.js
+++ client/index.js
@@ -6,6 +6,9 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  // 
+  // 1. grab the proof from the MerkleTree
+  // 2. send the proof and the leaf (exact name on the list)
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
